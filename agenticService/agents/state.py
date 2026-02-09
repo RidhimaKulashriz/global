67357641b49de5670621ac21fe12ae49ed2869f2
Govind S <govindsys1008@gmail.com>
2026-02-09 23:13:44 +0530

    Weather node done

--- agenticService/agents/state.py
+++ agenticService/agents/state.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional,List
+from typing import Optional,List,Dict, Any
 class State(BaseModel):
     user_obj:Optional[str]=None
     user_url:Optional[str]=None
     location: Optional[str]=None
     caption:Optional[str]=None
     news:Optional[List[str]]=None
-    weather:Optional[str]=None
+    weather:Optional[Dict[str, Any]]=None
     segregator:Optional[str]=None
     verdict:Optional[str]=None
     prob:Optional[float]=0
--- agenticService/agents/weather_node.py
+++ agenticService/agents/weather_node.py
@@ -1,5 +1,9 @@
 from .state import State
+import requests
 def weather_node(state: State):
     print("WEATHER CALLED")
-    return {"weather": "WEATHER NODE"}
+    city = state.location
+    resp = requests.get(f"https://still-block-2193.govindsys1008.workers.dev?city={city}")
+    if resp.status_code==200:
+        return {"weather": resp.json()} 
 
