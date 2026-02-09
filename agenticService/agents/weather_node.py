1b889a3a5333fdb0031b53a56b9b3f9289c9e852
Govind S <govindsys1008@gmail.com>
2026-02-09 23:16:26 +0530

    Fixed exposed open endpoint

--- agenticService/agents/weather_node.py
+++ agenticService/agents/weather_node.py
@@ -1,9 +1,12 @@
 from .state import State
 import requests
+from dotenv import load_dotenv
+load_dotenv()
 def weather_node(state: State):
+    BASE_URL = os.getenv("BASE_URL")
     print("WEATHER CALLED")
     city = state.location
-    resp = requests.get(f"https://still-block-2193.govindsys1008.workers.dev?city={city}")
+    resp = requests.get(f"{BASE_URL}?city={city}")
     if resp.status_code==200:
         return {"weather": resp.json()} 
 
